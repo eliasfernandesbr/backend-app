068b151d159a68d3a9ca69020e6e1f23f9e45fa7
elias fernandes <elias@boxgroup.net.br>
2021-08-01 20:36:48 -0300

    add .editorconfig

--- src/routes/firstRoute.ts
+++ src/routes/firstRoute.ts
@@ -1,6 +1,5 @@
 import { Request, Response } from "express";
 
-export function firstRoute(request: Request, response: Response){
-    return response.json({message: 'Hello WOrld'})
+export function firstRoute(request: Request, response: Response) {
+  return response.json({ message: "Hello WOrld" });
 }
-
--- src/server.ts
+++ src/server.ts
@@ -1,13 +1,12 @@
-import express from 'express';
-import { firstRoute } from './routes/firstRoute';
+import express from "express";
+import { firstRoute } from "./routes/firstRoute";
 // import 'dotenv/config';
 // const PORT = process.env.APP_PORT;
 
 const app = express();
 
-app.get('/', firstRoute)
+app.get("/", firstRoute);
 
-
-app.listen(3333, ()=>{
-    console.log(`O SERVIDOR ESTÁ RODANDO NA PORTA 3333`)
-})
+app.listen(3333, () => {
+  console.log(`O SERVIDOR ESTÁ RODANDO NA PORTA 3333`);
+});
